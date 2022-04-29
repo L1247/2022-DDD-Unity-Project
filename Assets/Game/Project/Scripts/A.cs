f70e1e6b2f5e25effede3d6e5785dd7a91af520f
rStar <starlove124760@gmail.com>
2022-04-30 05:05:21 +0800

    add Test

---
+++ Assets/Game/Project/Scripts/A.cs
@@ -0,0 +1,14 @@
+namespace Game.Project.Scripts
+{
+    public class A
+    {
+    #region Public Methods
+
+        public int B()
+        {
+            return 1;
+        }
+
+    #endregion
+    }
+}
